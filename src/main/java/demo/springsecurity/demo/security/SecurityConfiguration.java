713d370c040fe3f953b3400d72af05a750a34165
Duc <vanduc2514@gmail.com>
2020-06-22 22:20:37 +0700

    1. Cài đặt phân quyền cho trang login và trang chủ

--- src/main/java/demo/springsecurity/demo/security/SecurityConfiguration.java
+++ src/main/java/demo/springsecurity/demo/security/SecurityConfiguration.java
@@ -10,6 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").access("hasRole('ROLE_ADMIN')");
+        //Chặn các request vào trang chủ phải có role admin
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        //Cho phép các request vào trang Login được vào thoải mái
+        http.formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password");
+        //Sử dụng trang login của mình thay vì sử dụng trang login có sẵn. Cài đặt spring security kiểm tra password ở
+        //2 ô input có name = username và name = password (param của form)
     }
 
     @Override
